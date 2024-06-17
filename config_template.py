51d45223e80edcdc96b595cb67288185e37bfba9
Mitja Rislakki <57709659+mitjarislakki@users.noreply.github.com>
2024-06-17 17:11:37 +0300

    add WIP config file

---
+++ config_template.py
@@ -0,0 +1,19 @@
+# Process switch
+REPLY_TAG = "nc:rpc-reply"
+DATA_TAG = "data"
+
+# LLDP Sniffer
+INTERF = None
+
+# NETCONF connection
+HOST = None
+USERNAME = None
+PASSWORD = None
+PORT = None
+
+# Network map
+RESIDENCE_TIME = 800  # in nanosec
+
+# Traffic shaper / frame handler
+CYCLE_TIME = 100
+LINK_DELAY = 420000  # 0.42ms
